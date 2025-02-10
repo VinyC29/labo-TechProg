8c27db16b98a1eba1a0c5d3a909c54ea625a1a49
Vincent <121204771+TheLastViny@users.noreply.github.com>
2025-02-10 16:00:17 -0500

    LabS1J1

--- Sources/labo.h
+++ Sources/labo.h
@@ -4,61 +4,30 @@
 * Déclarer une structure de donnée qui va s'appeler String. La structure va avoir une propriété characters et une propriété length.
 */
 
-typedef struct string
+struct String
 {
-	char* characters;
+	char characters;
 	int length;
-}String;
+};
 
 
 /*
 * Faire une fonction qui retourne a sous-tableau de character de celui passer. 
 */
-char* sousTexte(char* texte, int from, int to) {
-	static char sub[256] = { 0 };
-	int j = 0;
-
-	for (int i = from; i < to; i++) {
-		sub[j] = texte[i];
-		j++;
-	}
-	sub[j] = '\0';
-	return sub;
-}
+char* sousTexte(char* texte, int from, int to);
 
 
 /*
 * Faire une fonction qui copie le contenu du tableau de character dans la propriété de data
 * de la structure string.
 */
-void set_String(char* text, String* s) {
-	s->characters = text;
-	s->length = strlen(text);
-}
+void set_String(char* text, String* s);
 
 /*
 * Faire une fonction qui vérifie si un mot est un palindrome i.e. kayak, racecar...
 * Si le mot est un palindrome retourner 1 sinon 0
 */
-int String_is_palindrome(String* s) {
-
-	if (s->length == 0 || s->length == 1) {
-		return 1; 
-	}
-
-	int left = 0;                      
-	int right = s->length - 1;
-
-	while (left < right) {
-		if (s->characters[left] != s->characters[right]) {
-			return 0; 
-		}
-		left++;
-		right--;
-	}
-
-	return 1; 
-}
+int String_is_palindrome(String* s);
 
 /*
 * Implémenter la fonction bubble sort et trier le tableau
@@ -66,9 +35,9 @@ int String_is_palindrome(String* s) {
 void bubbleSort(int elements[], int n)
 {
 	bool echanger = false;
-
+	
 	for (int i = 0; i <= n - 1; i++) {
-
+		
 		if (elements[i] > elements[i + 1]) {
 			echanger = true;
 			int temp = elements[i];
