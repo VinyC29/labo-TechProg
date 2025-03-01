f7e7b59e180f5edba7018e18d92b668e02682b3c
VinyC29 <2430095@bart.ca>
2025-02-28 23:40:05 -0500

    s4j2

--- labS4J2/Sources/labo.c
+++ labS4J2/Sources/labo.c
@@ -35,10 +35,54 @@ Node* peek(Queue* q) {
 
 
 void pushAsPriorityQueue(Queue* q, Node* n) {
+    Queue tempQueue = { NULL, NULL }; 
 
+    while (q->next != NULL && ((Person*)peek(q)->data)->age <= ((Person*)n->data)->age) {
+        push(&tempQueue, pop(q));
+    }
+
+    push(&tempQueue, n);
+
+    while (q->next != NULL) {
+        push(&tempQueue, pop(q));
+    }
+
+    while (tempQueue.next != NULL) {
+        push(q, pop(&tempQueue));
+    }
 }
 
 
 void sortQueue(Queue* q) {
+    Queue tempQueue = { NULL, NULL }; 
 
+    while (q->next != NULL) {
+        Node* minNode = pop(q);
+        Node* current = pop(q);
+        Node* tempStack = NULL;
+
+        while (current != NULL) {
+            if (((Person*)current->data)->age < ((Person*)minNode->data)->age) {
+                if (tempStack) push(q, tempStack);
+                tempStack = minNode;
+                minNode = current;
+            }
+            else {
+                push(q, current);
+            }
+            current = pop(q);
+        }
+
+        push(&tempQueue, minNode);
+
+       
+        while (tempStack) {
+            push(q, tempStack);
+            tempStack = pop(q);
+        }
+    }
+
+    while (tempQueue.next != NULL) {
+        push(q, pop(&tempQueue));
+    }
 }
